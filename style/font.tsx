f1c466f412f89241cb28aed5f1f039e4cc86e4b9
icsbillwei <shuheng.wei@outlook.com>
2024-05-17 23:48:14 -0400

    fonts

---
+++ style/font.tsx
@@ -0,0 +1,10 @@
+import { useFonts } from 'expo-font';
+
+
+const [fontsLoaded] = useFonts({
+    'Epilogue-300': require('../assets/Epilogue-300.ttf'),
+    'Epilogue': require('../assets/Epilogue.ttf'),
+    'Epilogue-500': require('../assets/Epilogue-500.ttf'),
+    'Epilogue-600': require('../assets/Epilogue-600.ttf'),
+    'Epilogue-700': require('../assets/Epilogue-700.ttf'),
+  });
