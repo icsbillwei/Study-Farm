6994912cfebaf8fa8db0df71a1ba0e480b965ba7
FionaChen0625 <fionachen645@gmail.com>
2024-05-18 03:03:48 -0400

    Added nav bar

--- app/farm.tsx
+++ app/farm.tsx
@@ -1,13 +1,17 @@
+import BottomNavBar from "@/components/Navbar";
 import { Link } from "expo-router";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet} from "react-native";
 
 export default function FarmScreen() {
-  return <View>
-    <Text>This is Farm Page!</Text>
-    <Text>This is Farm Page!</Text>
-    <Text>This is Farm Page!</Text>
-    <Text>This is Farm Page!</Text>
-    <Link replace href="/farm">Go to Farm</Link>
-    <Link replace href="/study">Go to Study</Link>
+  return <View style={styles.container}>
+    <Text >This is Farm Page!</Text>
+    <BottomNavBar />
   </View>
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+    width: "100%"
+  }
+});
